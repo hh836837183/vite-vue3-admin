bc2224102c0c5fdf6aeff62a7a4c18c773e36013
buqiyuan <buqiyuan@users.noreply.github.com>
2022-02-09 14:02:06 +0800

    Update ghPages 10540eb1de36f4cd6048f86e5b0363109d571760

---
+++ assets/settings-legacy.92ad5e78.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy.3848137e.js","./vendor-legacy.5dcc4181.js"],(function(e){"use strict";var n,t,c;return{setters:[function(e){n=e._},function(e){t=e.b,c=e.c}],execute:function(){e("default",n({},[["render",function(e,n){return t(),c("div",null," 个人中心 ")}]]))}}}));
---
+++ assets/settings.65f4efb6.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.cbbca19b.js";import{b as e,c as a}from"./vendor.75aebe02.js";var n=r({},[["render",function(r,n){return e(),a("div",null," 个人中心 ")}]]);export{n as default};
